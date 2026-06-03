a154ef076a77fbf4dc1efb3b2f1c75fb4d63d7b6
root <root@localhost.localdomain>
2026-06-03 20:31:10 +0800

    mylog工具的实现

---
+++ src/until/mylog.cc
@@ -0,0 +1,36 @@
+#include"../../include/until/mylog.h"
+#include<memory>
+namespace ns_logger
+{
+    std::shared_ptr<spdlog::logger> Logger::_logger = nullptr;
+    std::mutex Logger::_mutex;
+    void Logger::initLogger(std::string& loggerName , std::string& logFile , spdlog::level::level_enum logLevel)
+    {
+        if(_logger == nullptr)
+        {
+            std::lock_guard<std::mutex> lock(_mutex);
+            if(_logger == nullptr)
+            {
+                spdlog::flush_on(logLevel);
+                spdlog::init_thread_pool(8192 , 1);
+
+                if(logFile == "stdout")
+                {
+                    _logger = spdlog::stdout_color_mt(loggerName);
+                }
+                else
+                {
+                    _logger = spdlog::basic_logger_mt<spdlog::async_logger>(loggerName , logFile);
+                }
+
+                _logger->set_pattern("%H:%M [%n][%L] %v");
+                _logger->set_level(logLevel);
+            }
+        }
+    }
+    std::shared_ptr<spdlog::logger> Logger::logInfo()
+    {
+        return _logger;
+    }
+    
+}
