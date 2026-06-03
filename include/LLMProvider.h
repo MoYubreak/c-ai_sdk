61bae511097ce2cda6af47d8dc7f515653b51ad4
root <root@localhost.localdomain>
2026-06-03 20:44:07 +0800

    ai模型基类的设计

---
+++ include/LLMProvider.h
@@ -0,0 +1,25 @@
+#pragma once 
+#include<string>
+#include<map>
+#include<function>
+#include<vector>
+#include"common.h"
+
+namespace ai-chat-sdk
+{
+    class LLMProvider
+    {
+    public:
+        virtual void initModel(const std::map<std::string , std::string>& modelConfig) = 0;
+        virtual bool isAvailable() const = 0;
+        virtual std::string getModelName() const = 0;
+        virtual std::string getModelDesc() const =0;
+        virtual void sendMessage(const std::vector<Message>& messages , const std::map<std::string , std::string>& requestParam) = 0;
+        virtual void sendMessageStream(const std::vector<Message>& messages , const std::map<std::string , std::string>& requestParam , \
+            std::function<void(const std::string& , bool)> callback) = 0;
+    private:
+        bool _isAvailable = false;
+        std::string _apiKey;
+        std::string _endpoint;
+    };
+}
